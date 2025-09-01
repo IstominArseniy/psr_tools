e7e969f53533cb1e82fd15f1d1d63619f6971cad
IstominArseniy <istomin.aiu@phystech.edu>
2025-09-01 16:54:25 +0300

    update 2D density profile class. Needs to be cheked

--- psr_lib/interface/denisity_profile_class.py
+++ psr_lib/interface/denisity_profile_class.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.interpolate
 from matplotlib import pyplot as plt
+import json
 
 class DensityProfile1D:
     def __init__(self, x_arr, n_arr):
@@ -22,6 +23,8 @@ class DensityProfile1D:
 
     def _create_interpolation(self):
         return scipy.interpolate.make_interp_spline(self.x_arr, self.n_arr, k=1)
+        # return scipy.interpolate.RegularGridInterpolator((self.x_arr,), self.n_arr, method='linear', bounds_error=False, fill_value=0)
+    
 
     def get_n(self, x, normalized=False):
         x = np.array(x)
@@ -32,21 +35,53 @@ class DensityProfile1D:
         else:
             return self.interpolant(x) / self.multiplicity
     
-    def write_to_file(self, filename):
-        pass
+    def write_to_file(self, filename, normalized=False):
+        tmp_dict = dict()
+        tmp_dict['x_arr'] = self.x_arr.tolist()
+        tmp_dict['n_arr'] = self.n_arr.tolist()
+        with open (filename + '.json', 'w') as f:
+            json.dump(tmp_dict, f)
 
     def _find_multiplicity(self):
         scipy.integrate.trapezoid(self.n_arr, self.x_arr)
 
 class DenistyProfile2D:
-    def __init__(self, n_arr):
-        pass
+    def __init__(self, x_arr, phi_arr, n_arr):
+        x_arr = np.array(x_arr)
+        phi_arr = np.array(phi_arr)
+        n_arr = np.array(n_arr)
+        self.Npoints_x = n_arr.shape[0]
+        self.Npoints_phi = n_arr.shape[1]
+        self.x_arr = x_arr
+        self.phi_arr = phi_arr
+        self.n_arr = n_arr
+        self.interpolant = self._create_interpolation()
+        self.multiplicity = self._find_multiplicity()
 
     def show(self):
         pass
 
-    def get_1D_slice(self, phi):
-        pass
+    def get_n(self, x, phi):
+        return self.interpolant(x, phi)
+
+    def get_1D_slice(self, phi, Npoints=None):
+        if Npoints is None:
+            Npoints=self.Npoints_x
+        xs = np.linspace(self.x_arr[0], self.x_arr[-1], Npoints)
+        ns = self.get_n(xs, phi)
+        return DensityProfile1D(xs, ns)
+
+    def write_to_file(self, filename, normalized=False):
+        tmp_dict = dict()
+        tmp_dict['x_arr'] = self.x_arr.tolist()
+        tmp_dict['phi_arr'] = self.phi_arr.tolist()
+        tmp_dict['n_arr'] = self.n_arr.tolist()
+        with open (filename + '.json', 'w') as f:
+            json.dump(tmp_dict, f)
 
     def _create_interpolation(self):
+        x_grid, phi_grid = np.meshgrid(self.x_arr, self.phi_arr, indexing='ij')
+        return scipy.interpolate.RegularGridInterpolator((x_grid, phi_grid), self.n_arr, method='linear', bounds_error=False, fill_value=0)
+    
+    def _find_multiplicity(self):
         pass
