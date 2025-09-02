524052c738d49d4f73916218845e8aede8e0cbfa
IstominArseniy <istomin.aiu@phystech.edu>
2025-09-02 15:13:05 +0300

    add paralllelization to find_density_profile_1D routine

--- psr_lib/plasma_production/density_calculation.py
+++ psr_lib/plasma_production/density_calculation.py
@@ -3,19 +3,25 @@ from tqdm.autonotebook import tqdm
 import numpy as np
 import warnings
 from ..interface import DensityProfile1D
+# from multiprocessing import Pool
+from tqdm.contrib.concurrent import process_map
+from functools import partial
 
-def find_1D_density_profile(psr:RadioPulsar, gamma_arr, Npoints=10) -> DensityProfile1D:
+def _find_multiplicity_1D(x, psr:RadioPulsar, gamma_func:callable):
+    return psr.curvature_multipliticy(x, gamma_func(x))
+
+def find_density_profile_1D(psr:RadioPulsar, gamma_func:callable, Npoints=10, max_workers=12) -> DensityProfile1D:
     #TODO multiprocessing, better gamma_arr
     x_min = 0.05
     n_arr = np.zeros(Npoints)
     x_arr = np.linspace(x_min, 1, Npoints)
     with warnings.catch_warnings(record=True, ) as warning_list:
-        for i in tqdm(range(Npoints)):
-            n_arr[i] = psr.curvature_multipliticy(x_arr[i], gamma_arr[i])
+        worker = partial(_find_multiplicity_1D, psr=psr, gamma_func=gamma_func)
+        n_arr = process_map(worker, x_arr, max_workers=max_workers)
     if len(warning_list) != 0:
         print(f"{len(warning_list)} warnings were catched!")
     return DensityProfile1D(x_arr, n_arr)
 
 
-def find_2D_density_profile(psr):
+def find_density_profile_2D(psr):
     pass
