add simple acc. potential calculation, bug fixes

psr_tools/interface/psr_class.py
@@ -93,7 +93,6 @@ class RadioPulsar():
         return: curvature radius
         """
         EPS = self.R**2 / self.R0 / self.RLC
-        EPS=0
         Rc_cm =  self.KRc * 4/3 * self.R**2 / self.R0 / (x + EPS) * np.sqrt(r)
         if units == 'cm':
             return Rc_cm
@@ -104,13 +103,13 @@ class RadioPulsar():
         else:
             raise ValueError("Incorrect units name.")
 
-    def hRS(self, r, phi, units='cm'):
+    def hRS(self, x, phi, units='cm'):
         """
         Ruderman-Sutherlend height
         units can be cm, R0 and R
         """
-        cs=np.abs(np.cos(self.chi-1.5 * np.sin(phi) * np.sin(self.chi) * r * self.R0/ self.R ))
-        h_cm = 1.1e2 * cs**(-3/7) * self.Rc(r, units='cm')**(2/7) * self.P**(3/7) / self.B_surf12**(4/7)
+        cs=np.abs(np.cos(self.chi-1.5 * np.sin(phi) * np.sin(self.chi) * x * self.R0/ self.R ))
+        h_cm = 1.1e2 * cs**(-3/7) * self.Rc(x, 1, units='cm')**(2/7) * self.P**(3/7) / self.B_surf12**(4/7)
         if units == 'cm':
             return h_cm
         elif units == 'R0':

psr_tools/plasma_production/density_calculation.py
@@ -8,32 +8,35 @@ from tqdm.contrib.concurrent import process_map
 from functools import partial
 from itertools import product
 
-def _find_multiplicity_1D(x, psr:RadioPulsar, gamma_func:callable):
+def _find_multiplicity_1D(psr:RadioPulsar, x, gamma_func:callable):
     return psr.curvature_multipliticy(x, gamma_func(x))
 
-def _find_multiplicity_2D(x_phi_pair, psr:RadioPulsar, gamma_func:callable):
+def _find_multiplicity_2D(psr:RadioPulsar, x_phi_pair, gamma_func:callable):
     return psr.curvature_multipliticy(x_phi_pair[0], gamma_func(x_phi_pair[0], x_phi_pair[1]))
 
-def find_density_profile_1D(psr:RadioPulsar, gamma_func:callable, Npoints=10, max_workers=12) -> DensityProfile1D:
+def find_density_profile_1D(psr:RadioPulsar, gamma_func:callable, Npoints=10) -> DensityProfile1D:
     x_min = 0.05 # TODO do smth with x_min
-    x_arr = np.linspace(x_min, 1, Npoints)
+    x_arr = np.linspace(x_min, 1-x_min, Npoints)
     n_arr = np.zeros(Npoints)
     with warnings.catch_warnings(record=True) as warning_list:
-        worker = partial(_find_multiplicity_1D, psr=psr, gamma_func=gamma_func)
-        n_arr = process_map(worker, x_arr, max_workers=max_workers)
+        # worker = partial(_find_multiplicity_1D, psr=psr, gamma_func=gamma_func)
+        # n_arr = process_map(worker, x_arr, max_workers=max_workers)
+        for ind, x in enumerate(x_arr):
+            n_arr[ind] = _find_multiplicity_1D(psr, x, gamma_func)
     if len(warning_list) != 0:
         print(f"{len(warning_list)} warnings were catched!")
     return DensityProfile1D(x_arr, n_arr)
 
+# REDO 2D
 
-def find_density_profile_2D(psr:RadioPulsar, gamma_func:callable, Npoints_x=10, Npoints_phi=10, max_workers=12) -> DensityProfile2D:
-    x_min = 0.05
-    x_arr = np.linspace(x_min, 1, Npoints_x)
-    phi_arr = np.linspace(0, 2 * np.pi, Npoints_phi, endpoint=False)
-    n_arr = np.zeros((Npoints_x, Npoints_phi))
-    with warnings.catch_warnings(record=True) as warning_list:
-        worker = partial(_find_multiplicity_2D, psr=psr, gamma_func=gamma_func)
-        n_arr = np.array(process_map(worker, list(product(x_arr, phi_arr)), max_workers=max_workers)).reshape(Npoints_x, Npoints_phi)
-    if len(warning_list) != 0:
-        print(f"{len(warning_list)} warnings were catched!")
-    return DensityProfile2D(x_arr, phi_arr, n_arr)
+# def find_density_profile_2D(psr:RadioPulsar, gamma_func:callable, Npoints_x=10, Npoints_phi=10, max_workers=12) -> DensityProfile2D:
+#     x_min = 0.05
+#     x_arr = np.linspace(x_min, 1, Npoints_x)
+#     phi_arr = np.linspace(0, 2 * np.pi, Npoints_phi, endpoint=False)
+#     n_arr = np.zeros((Npoints_x, Npoints_phi))
+#     with warnings.catch_warnings(record=True) as warning_list:
+#         worker = partial(_find_multiplicity_2D, psr=psr, gamma_func=gamma_func)
+#         n_arr = np.array(process_map(worker, list(product(x_arr, phi_arr)), max_workers=max_workers)).reshape(Npoints_x, Npoints_phi)
+#     if len(warning_list) != 0:
+#         print(f"{len(warning_list)} warnings were catched!")
+#     return DensityProfile2D(x_arr, phi_arr, n_arr)